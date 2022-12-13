565b2be7d34be08a0ca317950fccbc6023447f0b
kimchaeeun3447 <rlacodms3447@naver.com>
2022-12-14 04:15:31 +0900

    [feature/#1] MySQL 서버 연결 틀 작성
    - MySQL 서버 배포후에 .env에 HOST, USER, PASSWORD, PORT, DATABASE 명만 넣으면 작동하도록 만들었음
    - service로직에 주석처리해둠
    - 예시는 게시물 상세보기 로직에 있음

--- src/service/postService.js
+++ src/service/postService.js
@@ -1,3 +1,9 @@
+/**  DB 연결 부분 - MySQL 서버 파면 주석 제거하기 **/
+//import dbConfig from "../db/dbConfig.js";
+// const pool = await dbConfig;
+// const connection = await pool.getConnection();
+
+
 //전체 게시글 리스트 불러오기  GET /api/post
 const getPost = async() => {
     return;
@@ -10,6 +16,13 @@ const postPost = async() => {
 
 //게시글 상세보기  GET /api/post/:postId
 const getPostDetail = async() => {
+    /**  DB 연결 부분 - MySQL 서버 파면 주석 제거하기 **/
+    // const query = 'SELECT * FROM user';
+    // const result = await connection.query(query);
+    // //Connection 할당 해제
+    // connection.release();
+    //console.log(result);
+
     return '안녕';
 };
 
