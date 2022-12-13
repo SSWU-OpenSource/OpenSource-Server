9cf406e007a5da600594148f06a8d02798381bc4
kimchaeeun3447 <rlacodms3447@naver.com>
2022-12-13 22:46:55 +0900

    [feature/#1] express 서버 초기 세팅
    src/index.js, nodemon.json, package.json, .gitignore 작성

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.send('express server');
+});
+
+app.listen(8000, () => {
+    console.log(`
+          #############################################
+              Server listening on port: 8000
+          #############################################
+      `);
+});
