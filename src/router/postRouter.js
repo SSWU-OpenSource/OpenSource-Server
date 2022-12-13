b4dedd583f5722b1917e9543cd13566888908554
kimchaeeun3447 <rlacodms3447@naver.com>
2022-12-13 23:36:40 +0900

    [feature/#1] 라우터 컨트롤러 연결
    라우터 마다 호출할 컨트롤러 연결

--- src/router/postRouter.js
+++ src/router/postRouter.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
+import postController from '../controller/postController';
 
 const router = Router();
 
 //전체 게시글 리스트 불러오기  GET /api/post
-router.get('/', );
+router.get('/', postController.getPost);
 
 //게시글 작성하기  POST /api/post
-router.post('/', );
+router.post('/', postController.postPost);
 
 //게시글 상세보기  GET /api/post/:postId
-router.get('/:postId', );
+router.get('/:postId', postController.getPostDetail);
 
 
 module.exports = router;
--- src/router/scrapRouter.js
+++ src/router/scrapRouter.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
+import scrapController from '../controller/scrapController';
 
 const router = Router();
 
 //스크랩하기  POST /api/scrap
-router.post('/', );
+router.post('/', scrapController.postScrap);
 
 //스크랩한 게시글 리스트 가져오기  GET /api/scrap
-router.get('/', );
+router.get('/', scrapController.getScrap);
 
 //스크랩 여부 가져오기  GET /api/scrap/:postId
-router.get('/:postId', );
+router.get('/:postId', scrapController.getIsScrap);
 
 
 module.exports = router;
