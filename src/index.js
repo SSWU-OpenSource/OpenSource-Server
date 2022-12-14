b904d4849bcd87a964131f2eab826b3d76375320
Chae Eun Kim <78145403+kimchaeeun3447@users.noreply.github.com>
2022-12-14 17:01:11 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res, next) => {
 app.listen(3000, () => {
     console.log(`
           #############################################
-              Server listening on port: 8000
+              Server listening on port: 3000
           #############################################
       `);
 });
