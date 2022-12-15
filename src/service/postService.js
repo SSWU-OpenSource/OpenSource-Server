38a17be624b15dbdcf3633ea1bfad575096cf3dc
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2022-12-15 18:42:23 +0900

    [chore] delete db connection code

--- src/service/postService.js
+++ src/service/postService.js
@@ -1,7 +1,3 @@
-/**  DB 연결 부분 - MySQL 서버 파면 주석 제거하기 **/
-import dbConfig from "../db/dbConfig.js";
-
-
 //전체 게시글 리스트 불러오기  GET /api/post
 const getPost = async() => {
     return;
@@ -14,16 +10,7 @@ const postPost = async() => {
 
 //게시글 상세보기  GET /api/post/:postId
 const getPostDetail = async(postId) => {
-    const pool = await dbConfig;
-    const connection = await pool.getConnection();
-    const query = 'SELECT * FROM postTbl';
-
-    const result = await connection.query(query, postId);
-    console.log(result);
-    //Connection 할당 해제
-    connection.release();
-
-    return '안녕';
+    return;
 };
 
 
